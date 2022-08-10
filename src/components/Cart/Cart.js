e2e0408582d24ba8cd75274fcac263398bf2939e
mdubo <dubokovic7@gmail.com>
2022-08-10 17:09:14 -0300

    orden de compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,10 +3,29 @@ import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
 import ItemCart from '../ItemCart/itemCart';
 import '../Cart/cart.css';
+import { addDoc, getFirestore, collection } from 'firebase/firestore';
 
 
 const Cart = () => {
   const {cart, totalPrice } = useCartContext();
+
+  const orden = {
+    buyer: {
+      name: 'Marcio',
+      email: 'dubokovic7@gmail.com',
+      phone: '777777',
+      adress: 'sarasasarasa'
+    },
+    items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+    total: totalPrice(),
+  }
+
+  const handleClick = () => {
+
+    const db = getFirestore();
+    const ordersCollection = collection(db, 'ordenes');
+    addDoc(ordersCollection, orden).then(({ id }) => console.log(id))
+  }
   
   if (cart.length === 0){
     return (
@@ -24,6 +43,7 @@ const Cart = () => {
       <h1 id='#total'>
           Total: $ {totalPrice()} Ars.
       </h1>
+      <h1><button className='fincom' onClick={handleClick}>Finalizar Compra.</button></h1>
     </>
   )
 }
